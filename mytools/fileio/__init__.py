008bcfd8a778dcec6bd46d2bb53e80c9b76fc7f3
Yaopeng-PC <mailmyps@gmail.com>
2020-05-01 14:10:33 +0300

    Add a function to save siganls to txt file

--- mytools/fileio/__init__.py
+++ mytools/fileio/__init__.py
@@ -1,3 +1,3 @@
 # __all__ = ["fileio"]
 from .textio import embla_csv, alice_csv, read_rri, somnoscreen_csv
-from .edfio import  get_dataindex_from_edf, get_signal_from_edf
+from .edfio import get_signal_from_edf, save_signal
--- mytools/fileio/edfio.py
+++ mytools/fileio/edfio.py
@@ -1,4 +1,7 @@
 import numpy as np
+import pyedflib
+import os
+import sys
 
 
 def get_dataindex_from_edf(edffile, targetstr, sensor="Thermistor"):
@@ -26,30 +29,63 @@ def get_dataindex_from_edf(edffile, targetstr, sensor="Thermistor"):
         return signal_index
 
 
-def get_signal_from_edf(edffile, index):
+def get_signal_from_edf(edffile, index, isclose=True):
     """
     get signal and frequency from edffile
     input:
-        EdfReader    : edffile
-        int(or list) : channal's index
+        EdfReader(or str) : edffile
+        int, str(or list) : channal's index
+        isclose           : only when edffile is a string, this is useful, if it is true, the edffile will be closed
+                            else, the edfreader will be also returned
     return:
         list : signals
         list : frequencys
+        list : labels
 
     """
+    if type(edffile) == str:
+        edffile = pyedflib.EdfReader(edffile)
+        signal, freq, label = get_signal_from_edf(edffile, index)
+        if isclose:
+            edffile._close()
+            return signal, freq, label
+        else:
+            return signal, freq, label, edffile
     if type(index) == int:
         return edffile.readSignal(index), edffile.getSignalHeader(index)['sample_rate'], edffile.getLabel(index)
     elif type(index) == str:
         newindex = get_dataindex_from_edf(edffile, index)
-        get_signal_from_edf(edffile, newindex)
+        return get_signal_from_edf(edffile, newindex)
     else:
-        if type(index[0]) == np.int:
+        if type(index[0]) == int:
             index_len = len(index)
             signals = np.empty(index_len, dtype=np.object)
             freq = np.empty(index_len)
-            label = np.empty(index_len, dtype=np.string_)
+            label = np.empty(index_len, dtype=np.object)
             for i, ii in enumerate(index):
                 signals[i], freq[i], label[i] = get_signal_from_edf(edffile, ii)
-            return signals, freq, label
+            return (signals, freq, label)
         else:
-            get_signal_from_edf(edffile, get_dataindex_from_edf(edffile, index))
+            index = get_dataindex_from_edf(edffile, index)
+            return get_signal_from_edf(edffile, index)
+
+
+def save_signal(signals, freqs, labels, patid, starttime=None):
+    path = "./" + patid + "/"
+    folder = os.path.exists(path)
+    if not folder:
+        os.makedirs(path)
+    if type(freqs) != np.ndarray:
+        f = open(path + labels + ".dat", "w")
+        old_stdout = sys.stdout
+        sys.stdout = f
+        if starttime is not None:
+            print(starttime)
+        time = np.arange(0, signals.size / freqs, 1/freqs)
+        for i in range(time.size):
+            print("%f \t %f" % (time[i], signals[i]))
+        sys.stdout = old_stdout
+        f.close()
+    else:
+        for i in range(freqs.size):
+            save_signal(signals[i], freqs[i], labels[i], patid, starttime)
