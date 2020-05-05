1e71d17bbf62139ca5b6265e2fb4055a565223ae
Yaopeng-PC <mailmyps@gmail.com>
2020-05-05 03:31:54 +0300

    add faster interpolate function and resample function

--- mytools/breathing_freq/breathing_freq.py
+++ mytools/breathing_freq/breathing_freq.py
@@ -3,7 +3,7 @@ My functions used to calculate breathing frequency
 """
 import numpy as np
 import biosppy.signals.tools as st
-from scipy.signal import hilbert
+from ..phase_sync.tools import phase
 from biosppy.signals.resp import resp
 
 
@@ -23,6 +23,16 @@ def freq_from_biosppy(signal, freq):
     return t, f
 
 
+def butter_filter(signal, freq, frequency=[0.1, 0.35], order=2):
+    signal, _, _ = st.filter_signal(signal=signal,
+                                    ftype='butter',
+                                    band="bandpass",
+                                    order=order,
+                                    frequency=frequency,
+                                    sampling_rate=freq)
+    return signal
+
+
 def freq_from_phase(signal, freq):
     """
     breathing frequency through phase method
@@ -34,17 +44,12 @@ def freq_from_phase(signal, freq):
         f(np.array)      : breathing frequency
 
     """
-    signal, _, _ = st.filter_signal(signal=signal,
-                                    ftype='butter',
-                                    band='bandpass',
-                                    order=2,
-                                    frequency=[0.1, 0.35],
-                                    sampling_rate=freq)
+    signal = butter_filter(signal, freq)
     # pass a filter to remove the noise.
     if freq >= 4:
         new_freq = 4
         windows_size = freq / new_freq
-        while(windows_size - int(windows_size) != 0):
+        while (windows_size - int(windows_size) != 0):
             new_freq = new_freq + 1
             windows_size = freq / new_freq
         windows_size = int(windows_size)
@@ -56,13 +61,13 @@ def freq_from_phase(signal, freq):
     threshold = sorted_signal[-int(sorted_signal.size * 0.2)] * 0.005
     # choose a threshold
 
-    phase = np.arctan2(signal, hilbert(signal))
-    phase[np.abs(signal) < threshold] = 0
+    Phase = phase(signal)
+    Phase[np.abs(signal) < threshold] = 0
     # Set the phase where the signal is under the threshold to zero
-    phase_diff = np.diff(phase)
-    fire_position = np.where(phase_diff < - 0.5 * np.pi)[0]
+    phase_diff = np.diff(Phase)
+    fire_position = np.where(phase_diff < -0.5 * np.pi)[0]
     # find where the breathing cycle start
-    fire_position = fire_position[phase[fire_position + 1] != 0]
+    fire_position = fire_position[Phase[fire_position + 1] != 0]
 
     fire_position = fire_position / new_freq
     # change index to seconds
--- mytools/signaltools.py
+++ mytools/signaltools.py
@@ -1,6 +1,10 @@
+"""
+Tools used to analysis signal
+"""
 import numpy as np
 from mkl_fft import fft
 from .phase_sync.tools import hilbert, phase
+from numba import jit
 
 
 def powerspectrum(signal, freq=1, removemean=True):
@@ -21,3 +25,91 @@ def powerspectrum(signal, freq=1, removemean=True):
     spectrum = np.abs(fft_result[:size//2]) ** 2
     f = np.fft.fftfreq(size, 1/freq)
     return f[:size//2], spectrum
+
+
+@jit
+def interp1dlinear(x, y, xx):
+    """
+    Linear interpolate 1-D function
+    Input:
+        x(numpy.array) : old x
+        y(numpy.array) : old y
+        xx(numpy.array) : new y
+    return:
+        (numpy.array)  : new y
+    """
+    result = np.zeros_like(xx)
+    i = 0
+    istart = 0
+    if xx[i] < x[0]:
+        while xx[i] <= x[0]:
+            result[i] = y[0]
+            i += 1
+    for j in range(i, result.size):
+        while x[istart] < xx[j]:
+            istart += 1
+            if istart == y.size:
+                break
+        if istart == y.size:
+            result[j:] = y[-1]
+            break
+        if x[istart] == xx[j]:
+            result[j] = y[istart]
+            continue
+        slope = (y[istart] - istart - 1]) / (x[istart] - x[istart - 1])
+        result[j] = y[istart - 1] + slope * (xx[j] - x[istart - 1])
+    return result
+
+
+@numba.njit
+def nfreq(signal, n=1):
+    """
+    n times the signal's frequency
+    """
+    if n == 1:
+        return signal
+    else:
+        new_signal = np.empty((signal.size - 1)* n + 1)
+        new_signal[::n] = signal
+        diff = np.diff(signal)
+        for i in range(1, n):
+            new_signal[i::n] = signal[:-1] + diff * i / n
+        return new_signal
+
+
+def resample(signal, old_freq, new_freq, method="interp"):
+    """
+    resample the signal from an old frequency to a new frequency
+    """
+    if old_freq == new_freq:
+        return signal
+    if old_freq < new_freq:
+        if new_freq % old_freq == 0:
+            return nfreq(signal, new_freq // old_freq)
+        else:
+            t_old = np.arange(0, 1/old_freq * signal.size, 1/old_freq)
+            t_new = np.arange(0, 1/new_freq * signal.size, 1/new_freq)
+            return interp1dlinear(t_old, signal, t_new)
+    else:
+        if method == "interp":
+            n = 0
+            #while n * old_freq % new_freq != 0:
+            #    n += 1
+            #signal = nfreq(signal, n)
+            while (n + old_freq) % new_freq != 0:
+                n += 1
+            if n != 0:
+                t1 = np.arange(0, signal.size / old_freq, 1 / old_freq)
+                t2 = np.arange(0, signal.size / old_freq, 1 / (old_freq + n))
+                signal = interp1dlinear(t1, signal, t2)
+            N = (old_freq + n) // new_freq
+        elif method == "nfreq":
+            n = 1
+            while n * old_freq % new_freq != 0:
+                n += 1
+            signal = nfreq(signal, n)
+            N = (old_freq * n) // new_freq
+        iend = signal.size - signal.size % (N)
+        signal = signal[:iend].reshape(-1, N)
+        #return signal
+        return np.mean(signal, axis=1)
